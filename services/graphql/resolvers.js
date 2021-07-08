9e265393625043b21dfd46c3687622d6ad8ac386
binh-developer <binhdlt.work@gmail.com>
2021-07-08 16:11:39 +0700

    Update and  add timeline to graphlq

--- services/graphql/resolvers.js
+++ services/graphql/resolvers.js
@@ -1,63 +1,124 @@
-const {admin} = require('../firebase/setup');
+const { admin } = require("../firebase/setup");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
-    roomMessages: () => {
+    /**
+     * Collection room-messages in Firebase
+     * @returns
+     */
+    roomMessages() {
       return admin
         .database()
-        .ref('room-messages')
-        .child('-McIE8aa4OhT21x2JhiP')
-        .once('value')
-        .then(snap => snap.val())
-        .then(val => Object.keys(val).map(key => val[key]));
+        .ref("room-messages")
+        .child("-McIE8aa4OhT21x2JhiP")
+        .once("value")
+        .then((snap) => snap.val())
+        .then((val) => Object.keys(val).map((key) => val[key]));
     },
 
+    /**
+     * Collection room-metadata in Firebase
+     * @returns
+     */
     roomMetadata: async () => {
       const roomMetadataResponse = await admin
         .database()
-        .ref('room-metadata')
-        .once('value')
-        .then(snap => snap.val());
+        .ref("room-metadata")
+        .once("value")
+        .then((snap) => snap.val());
 
       // Map data to typeDefs
       const keys = Object.keys(roomMetadataResponse);
-      const mapsKeys = keys.map(function (item) {
-        const roomData = roomMetadataResponse[item];
-        const graphqlRoom = {
-          createdAt: roomData.createdAt,
-          createdByUserId: roomData.createdByUserId,
-          roomAvatar: roomData.roomAvatar,
-          roomName: roomData.roomName,
-          roomId: item,
-          lastMessage: roomData.lastMessage.message,
-          lastMessageTime: roomData.lastMessage.createdAt,
-          lastMessageUserName: roomData.lastMessage.userName,
-          lastMessageUserId: roomData.lastMessage.userId,
-        };
-        return graphqlRoom;
-      });
-
+      const mapsKeys = keys.map((item) => ({
+        roomId: item,
+        ...roomMetadataResponse[item],
+      }));
       return mapsKeys;
     },
 
+    /**
+     * Collection user-metadata in Firebase
+     * @returns
+     */
     userMetadata: async () => {
       const userMetadataResponse = await admin
         .database()
-        .ref('user-metadata')
-        .once('value')
-        .then(snap => snap.val());
+        .ref("user-metadata")
+        .once("value")
+        .then((snap) => snap.val());
 
       // Map data to typeDefs
       const keys = Object.keys(userMetadataResponse);
       const mapsKeys = keys.map(function (item) {
         const userData = userMetadataResponse[item];
-        const graphqlUser = {
-          deviceId: userData.deviceId,
-          isSignedIn: userData.isSignedIn,
-          userId: item,
-        };
-        return graphqlUser;
+
+        if (_.isEmpty(userData.rooms)) {
+          return {
+            deviceId: userData.deviceId,
+            isSignedIn: userData.isSignedIn,
+            userId: item,
+          };
+        } else {
+          // Map room list
+          const room = Object.keys(userData.rooms).map((roomKey) => ({
+            roomId: roomKey,
+            join: userData.rooms[roomKey].join,
+          }));
+
+          return {
+            deviceId: userData.deviceId,
+            isSignedIn: userData.isSignedIn,
+            userId: item,
+            rooms: [...room],
+          };
+        }
       });
+
+      return mapsKeys;
+    },
+
+    /**
+     * Collection timeline in Firebase
+     */
+    timeline: async () => {
+      const timelineResponse = await admin
+        .database()
+        .ref("timeline")
+        .once("value")
+        .then((snap) => snap.val());
+
+      const keys = Object.keys(timelineResponse);
+      const mapsKeys = keys.map(function (item) {
+        const timelineData = timelineResponse[item];
+
+        if (_.isEmpty(timelineData.likes)) {
+          return {
+            timelineId: item,
+            createdAt: timelineData.createdAt,
+            imageURL: timelineData.imageURL,
+            status: timelineData.status,
+            userId: timelineData.userId,
+            userName: timelineData.userName,
+          };
+        } else {
+          const like = Object.keys(timelineData.likes).map((likeId) => ({
+            likeId: likeId,
+            userId: timelineData.likes[likeId].userId,
+          }));
+
+          return {
+            timelineId: item,
+            createdAt: timelineData.createdAt,
+            imageURL: timelineData.imageURL,
+            status: timelineData.status,
+            userId: timelineData.userId,
+            userName: timelineData.userName,
+            likes: [...like],
+          };
+        }
+      });
+
       return mapsKeys;
     },
   },
--- services/graphql/typeDefs.js
+++ services/graphql/typeDefs.js
@@ -1,4 +1,4 @@
-const {gql} = require('apollo-server');
+const { gql } = require("apollo-server");
 const typeDefs = gql`
   type roomMessages {
     createdAt: Float
@@ -8,28 +8,54 @@ const typeDefs = gql`
     userName: String
   }
 
+  type lastMessage {
+    createdAt: Float
+    message: String
+    userName: String
+    userId: String
+  }
+
   type roomMetadata {
     createdAt: Float
     createdByUserId: String
     roomAvatar: String
     roomName: String
     roomId: String
-    lastMessage: String
-    lastMessageTime: Float
-    lastMessageUserName: String
-    lastMessageUserId: String
+    lastMessage: lastMessage
+  }
+
+  type userMetadataRooms {
+    roomId: String
+    join: Boolean
   }
 
   type userMetadata {
     userId: String
     deviceId: String
     isSignedIn: Boolean
+    rooms: [userMetadataRooms!]
+  }
+
+  type likeStatus {
+    likeId: String
+    userId: String
+  }
+
+  type timeline {
+    timelineId: String
+    createdAt: Float
+    imageURL: String
+    status: String
+    userId: String
+    userName: String
+    likes: [likeStatus!]
   }
 
   type Query {
     roomMessages: [roomMessages]
     roomMetadata: [roomMetadata]
     userMetadata: [userMetadata]
+    timeline: [timeline]
   }
 `;
 module.exports = typeDefs;
